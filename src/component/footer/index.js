d042fa86d10d18ca1ec658caccecf35f17c5f036
Isa Gul <isa.gul@beyn.com.tr>
2020-01-06 18:57:30 +0300

    button click event was added into test

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Footer = (props) => {
     const {header, desc} = props;
+    const [count, setCount] = useState(0);
 
     if(!header) {
         return null;
+    }
+
+    function increaseCount(){
+        setCount(prevCount => prevCount + 1);
+        if (props.emitEvent){
+            props.emitEvent();
+        }
+    }
+
+    function decreaseCount(){
+        setCount(prevCount => prevCount - 1)
     }
     
     return (
         <div className="footerComponent">
             <h1 className="header">{header}</h1>
             <p className="desc">{desc}</p>
+            <button className="incrementButton" onClick = {increaseCount}>Increment</button>
+            <button className="decrementButton" onClick = {decreaseCount}>Decrement</button>
+            <p className="count">{count}</p>
         </div>
     )
 }
--- src/component/footer/spec.js
+++ src/component/footer/spec.js
@@ -9,14 +9,16 @@ const setUp = (props) => {
 };
 
 describe('Footer Component', () => {
-
     describe('Have props', () => {
         let component;
+        let mockFunc;
         beforeEach(() => {
+            mockFunc = jest.fn();
             const props = {
                 header: 'Test Header',
-                desc: 'Test Desc'
-            }
+                desc: 'Test Desc',
+                emitEvent: mockFunc
+            };
             component = setUp(props);
         });
 
@@ -34,6 +36,35 @@ describe('Footer Component', () => {
             let wrapper = findByClassName(component, 'desc');
             expect(wrapper.length).toBe(1);
         });
+
+        it('Should render a increment button', () => {
+            let wrapper = findByClassName(component, 'incrementButton');
+            expect(wrapper.length).toBe(1);
+        });
+
+        it('Should render a decrement button', () => {
+            let wrapper = findByClassName(component, 'decrementButton');
+            expect(wrapper.length).toBe(1);
+        });
+
+        it('Should render a count field', () => {
+            let wrapper = findByClassName(component, 'count');
+            expect(wrapper.length).toBe(1);
+        });
+
+        it('Should count is 0', () => {
+            let wrapper = findByClassName(component, 'count');
+            expect(wrapper.text()).toEqual('0');
+        });
+
+        describe('Count Functions', () => {
+            it('Should emit callback on click event', () => {
+                const wrapper = findByClassName(component, 'incrementButton');
+                wrapper.simulate('click');
+                const callback = mockFunc.mock.calls.length;
+                expect(callback).toBe(1);
+            });
+        })
     });
 
     describe('Have NO props', () => {
@@ -41,7 +72,7 @@ describe('Footer Component', () => {
         let component;
         beforeEach(() => {
             component = setUp();
-        })
+        });
 
         it('Should not render', () => {
             let wrapper = findByClassName(component, 'footerComponent');
