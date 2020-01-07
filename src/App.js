6fbfca6f346772d9aa842854194021de38279dc0
Isa Gul <isa.gul@beyn.com.tr>
2020-01-07 15:57:27 +0300

    app component was changed as class component add its tests were added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './component/header/index';
 import Footer from './component/footer/index';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Footer header="Posts" desc="Click the button to render props" />
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      count: 0
+    };
+  }
+
+  returnAsNumber(number){
+    return number + 1;
+  }
+
+  changeCountValue(){
+    this.setState({
+      count: 1
+    })
+  }
+
+  render(){
+    return (
+    <div className="appComponent">
+        <Header />
+        <Footer header="Posts" desc="Click the button to render props" />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/App.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from './App';
+import {findByClassName} from '../Utils/index';
+
+const setUp = () => {
+    const component = shallow(<App/>);
+    return component;
+};
+
+describe('App Component', () => {
+
+    let component;
+    let mockFn;
+
+    beforeEach(() => {
+        component = setUp();
+        mockFn = jest.fn();
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+    it('Should render without errors', () => {
+        const wrapper = findByClassName(component, 'appComponent');
+        expect(wrapper.length).toBe(1);
+    })
+
+    it('Should returnAsNumber works as expected', () => {
+        const instance = component.instance();
+        const newValue = instance.returnAsNumber(6);
+        expect(newValue).toBe(7);
+    });
+
+    it('Should changeCountValue works as expected', () => {
+        const instance = component.instance();
+        instance.changeCountValue();
+        const newState = instance.state.count;
+        expect(newState).toBe(1);
+    });
+
+    test('Should mock function works as expected', () => {
+        const mockImp = mockFn.mockImplementation(number => 42 + number);
+        const result = mockImp(1)
+        expect(result).toBe(43);
+    });
+})
